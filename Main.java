66b9d0834c839fb0741b2110c4ad7148d4aa0bd9
DJ-111 <67715744+DJ-111@users.noreply.github.com>
2024-07-10 13:42:26 -0400

    Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating objects from the Dog class
+        Dog dog01 = new Dog("Tina", "Jack Russell Terrier", 1, "White");
+        Dog dog02 = new Dog("Lola", "Cane Corso", 1, "Black");
+
+        // Displaying the details of the dogs
+        dog01.display();
+        dog02.display();
+    }
+}
